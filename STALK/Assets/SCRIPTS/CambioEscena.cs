6fd366575cb86f88de4e57a1cbc7a946c8f57c17
Unknown <guillerjack@hotmail.com>
2017-12-13 22:09:50 +0100

    Botton de Salida y Trigger al menú

---
+++ STALK/Assets/SCRIPTS/CambioEscena.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Character")
+        {
+            SceneManager.LoadScene("MENU");
+        }
+    }
+}
