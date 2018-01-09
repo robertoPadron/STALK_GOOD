8418b1870e31d4128bff62aafafe68fab6299a0d
Unknown <guillerjack@hotmail.com>
2018-01-09 20:52:23 +0100

    Correcion range de camaras y feedback del jugador.

---
+++ STALK/Assets/SCRIPTS/FLICKERING.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLICKERING : MonoBehaviour {
+
+    Light blueLight;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    void Start()
+    {
+        blueLight = GetComponent<Light>();
+        StartCoroutine(Flashing());
+    }
+
+    IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTime,maxWaitTime));
+            blueLight.enabled = ! blueLight.enabled;
+        }
+    }
+}
