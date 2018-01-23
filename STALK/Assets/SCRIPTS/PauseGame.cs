3910e9500aee4b136ced652d83d304d1a7569134
Unknown <guillerjack@hotmail.com>
2018-01-23 18:35:48 +0100

    Nuevo character. Nuevas particulas. Incorporado el menu de pausa

---
+++ STALK/Assets/SCRIPTS/PauseGame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+    public Transform Canvas;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+    public void Pause()
+        {
+                if (Canvas.gameObject.activeInHierarchy == false)
+                {
+                    Canvas.gameObject.SetActive(true);
+                }
+            else
+                {
+                    Canvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+                }
+        }
+    public void LoadScene(int level)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }
+    public void doquit()
+    {
+        Debug.Log("SALIR");
+        Application.Quit();
+    }
+}
