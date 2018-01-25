c8bb72172294244fc25447fb67c65d3fee23d219
Unknown <guillerjack@hotmail.com>
2018-01-25 03:20:05 +0100

    Menu Pausa y Luz Personaje

--- STALK/Assets/SCRIPTS/PauseGame.cs
+++ STALK/Assets/SCRIPTS/PauseGame.cs
@@ -2,29 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PauseGame : MonoBehaviour {
+public class PauseGame : MonoBehaviour
+{
 
-    public Transform Canvas;
+    public Transform canvas;
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Pause();
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
         }
     }
-    public void Pause()
-        {
-                if (Canvas.gameObject.activeInHierarchy == false)
-                {
-                    Canvas.gameObject.SetActive(true);
-                }
-            else
-                {
-                    Canvas.gameObject.SetActive(false);
-            Time.timeScale = 1;
-                }
-        }
     public void LoadScene(int level)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(level);
